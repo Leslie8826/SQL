41864b605936c150d228d3ce5610a1697569a37f
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-08 21:12:31 -0500

    Update dml.sql

--- Oracle/Project 1/5. Populating the database/dml.sql
+++ Oracle/Project 1/5. Populating the database/dml.sql
@@ -1,9 +1,21 @@
+/* 1ST TABLE TO POPULATE */
 /* POPULATING HR2_REGION */
 insert into hr2_region(region_id, region_name)
     select distinct region_id, region_name 
     from hr_stage
     where region_id is not null
 
-/* CHECKING THE DATA */
+/* OUTPUT FOR HR2_REGION */
 select * from hr2_region
 
+
+
+/* 2ND TABLE TO POPULATE */
+/* POPULATING HR2_COUNTRY */
+insert into hr2_country(country_id, country_name, region_id)
+    select distinct country_id, country_name, region_id
+    from hr_stage
+
+
+/* OUTPUT FOR HR2_COUNTRY */
+select * from hr2_country
