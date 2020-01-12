ab639f40506ac5af50e6061807fe1da36318a460
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-12 16:08:30 -0500

    Update dml.sql

--- Oracle/Project 1/5. Populating the database/dml.sql
+++ Oracle/Project 1/5. Populating the database/dml.sql
@@ -5,7 +5,7 @@ insert into hr2_region(region_id, region_name)
     from hr_stage
     where region_id is not null
 
-/* OUTPUT FOR HR2_REGION */
+/* OUTPUT FOR HR2_REGION CAN BE SEE WITH THE FOLLOWING */
 select * from hr2_region
 
 
@@ -16,7 +16,7 @@ insert into hr2_country(country_id, country_name, region_id)
     select distinct country_id, country_name, region_id
     from hr_stage
 
-/* OUTPUT FOR HR2_COUNTRY */
+/* OUTPUT FOR HR2_COUNTRY CAN BE SEE WITH THE FOLLOWING */
 select * from hr2_country
 
 
@@ -26,5 +26,15 @@ insert into hr2_location(location_id, street_address, postal_code, city, state_p
     select distinct location_id, street_address, postal_code, city, state_province, country_id
     from hr_stage
     
-/* OUTPUT FOR HR2_LOCATION */
+/* OUTPUT FOR HR2_LOCATION CAN BE SEE WITH THE FOLLOWING */
 select * from hr2_location
+
+
+/* 4TH TABLE TO POPULATE */
+/* POPULATING HR2_DEPARTMENT */
+insert into hr2_department(department_id, department_name, manager_id, location_id)
+    select distinct department_id, department_name, manager_id, location_id
+    from hr_stage
+
+/* OUTPUT FOR HR2_DEPARTMENT CAN BE SEE WITH THE FOLLOWING */
+select * from hr2_department
