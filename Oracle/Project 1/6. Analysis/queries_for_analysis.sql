4fc8e3dfc6d58528d5e34189ee1f99821bb7d682
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-13 23:38:08 -0500

    Delete queries_for_analysis.sql

--- Oracle/Project 1/6. Analysis/queries_for_analysis.sql
+++
@@ -1,8 +0,0 @@
-/* In this analysis, we query the database we created earlier */
-/* We also visualize the resulted data using tableau */
-/* See the screeshots of tableau for each query attached in the analysis folder */
-
-/* 1. Display details of jobs where the minimum salary is greater than 10000.*/
-select *
-from hr2_job
-where min_salary>10000
