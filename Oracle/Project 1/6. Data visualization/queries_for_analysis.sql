cd5e49bedf611e199558abe46a93ffcb027e8bd9
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-13 23:40:50 -0500

    Update queries_for_analysis.sql

--- Oracle/Project 1/6. Data visualization/queries_for_analysis.sql
+++ Oracle/Project 1/6. Data visualization/queries_for_analysis.sql
@@ -2,7 +2,13 @@
 /* We also visualize the resulted data using tableau */
 /* See the screeshots of tableau for each query attached in the analysis folder */
 
-/* 1. Display details of jobs where the minimum salary is greater than 10000.*/
+/* Q1. Display details of jobs where the minimum salary is greater than 10000.*/
 select *
 from hr2_job
 where min_salary>10000
+
+/* Q2. Display the first name and join date of the employees who joined between 1998 and
+1999.*/
+select first_name, hire_date 
+from hr2_employee
+where hire_date > '01-JAN-98' and hire_date < '31-DEC-99'
