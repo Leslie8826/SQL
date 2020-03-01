5d989d82e9b2a17d5220af8430257ae0f7335d15
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-03-01 11:11:06 -0500

    Update queries_for_analysis.sql

--- MySQL/Homework/Homework_1/4. Data visualization/Queries For Analysis/queries_for_analysis.sql
+++ MySQL/Homework/Homework_1/4. Data visualization/Queries For Analysis/queries_for_analysis.sql
@@ -1,1 +1,18 @@
+-- ----------------------------------------
+-- ----------------------------------------
+-- -------- DATA VISUALIZATION ------------
+-- ----------------------------------------
+-- ----------------------------------------
 
+
+# Q1 - How much does a bottle of morphine cost?
+select itemValue
+from item
+where itemDescription = 'bottle of morphine';
+
+
+
+# Q2 - Which volunteer(s), if any, do not live in New York (NY) or New Jersey (NJ) ?
+select *
+from volunteer
+where volunteerAddress not like '%NY' and volunteerAddress not like '%NJ'
